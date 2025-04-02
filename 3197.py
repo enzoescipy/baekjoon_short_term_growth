f0f7b222165cbd6c4b874e20e88c2d38606127bd
enzoescipy <enzoescipy@gmail.com>
2025-04-02 21:18:27 +0900

    brute_force case solved

--- 3197.py
+++ 3197.py
@@ -1,13 +1,14 @@
 import string
 
+from sympy import true
+from tomlkit import item
+
 
 class State:
-    ice = 0
+    ice = False
     ice_str = "X"
-    water = 1
+    water = True
     water_str = "."
-    swan = 2
-    swan_str = "L"
     
     @classmethod
     def str_to(cls, target):
@@ -15,8 +16,6 @@ class State:
             return cls.ice
         elif(target == cls.water_str):
             return cls.water
-        elif(target == cls.swan_str):
-            return cls.swan
         else:
             return None
 
@@ -28,13 +27,46 @@ class Tester:
     def purify_string_matrix(string_mat):
         res = []
         splitted = string_mat.split("\n")
-        for line in splitted:
-            items = list(line)
+        swan_pos = []
+        for i in range(len(splitted)):
+            items = list(splitted[i])
             symbolic_line = []
-            for item in items:
-                symbolic_line.append(State.str_to(item))
+            for j in range(len(items)):
+                item_res = State.str_to(items[j])
+                if item_res == None:
+                    swan_pos.append((i,j))
+                    item_res = True
+                symbolic_line.append(item_res)
             res.append(symbolic_line)
         
+        return (swan_pos, res)
+    
+    @staticmethod
+    def revert_purify(matrix):
+        res = ""
+        rows = len(matrix)
+        cols = len(matrix[0])
+        for i in range(rows):
+            for j in range(cols):
+                if (matrix[i][j] == False):
+                    res += "X"
+                else:
+                    res += "."
+            res += "\n"
+        return res
+    
+    @staticmethod
+    def pprint(matrix):
+        print("[")
+        for line in matrix:
+            print(line)
+        print("]")
+        
+    @staticmethod
+    def deepcopy(matrix):
+        res = []
+        for line in matrix:
+            res.append(line[:])
         return res
     
     def add_case(self, case):
@@ -43,26 +75,114 @@ class Tester:
     def run_cases(self, func):
         print("========case running ========")
         for case in self.cases:
+            print("case : ", case[0])
+            print(Tester.revert_purify((case[1])))
             res = func(case)
-            print("case : ")
-            print(case)
             print("res : ", res)
     
     def compare_cases(self, ans_func, target_func):
         print("========compare running ========")
         for case in self.cases:
+            print("ans : ", ans_res[0])
+            print(Tester.revert_purify((ans_res[1])))
+            print("target : ", target_res[0])
+            print(Tester.revert_purify((target_res[1])))
+            
             ans_res = ans_func(case)
             target_res = target_func(case)
-            print("ans : ")
-            print(ans_res)
-            print("target : ")
-            print(target_res)
-            
             print("is equal? : ", ans_res == target_res)
 
 def main_brute(case):
-    return str(case)
-
+    # case is the (swan_pos, pond)
+    # swan_pos is row to column position of the swan
+    # pond is the True/False array which False means swan can't cross that point
+    
+    swan_pos, pond = case
+    
+    # pond_copy = Tester.deepcopy(pond)
+    def pond_arify(pond):
+        """
+        make pond area visible by nominating each points.
+        WARNING : this function change the pond directly. 
+        e.g) [
+            [T,T,F],
+            [T,F,T],
+            [F,T,F],
+        ]
+        ->
+             [
+            [0,0,F],
+            [0,F,1],
+            [F,1,1],
+        ]
+        """
+        rows = len(pond)
+        cols = len(pond[0])
+        
+        def mark(i, j, num):
+            # mark position i, j in the pond. if already marked, return.
+            if (type(pond[i][j]) == type(True) and pond[i][j] == True):
+                pond[i][j] = num
+                # print("assign")
+            else:
+                # print("ret")
+                return
+            
+            # mark the 4-direction around self, and iterate
+            targets = [(i-1, j), (i, j-1), (i+1, j), (i, j+1)]
+            for target in targets:
+                if 0 <= target[0] and target[0] < rows and 0 <= target[1] and target[1] < cols:
+                    mark(target[0], target[1], num)
+                
+            
+        
+        count = 0
+        for i in range(rows):
+            for j in range(cols):
+                count += 1
+                if type(pond[i][j]) != type(True) or pond[i][j] != True:
+                    continue
+                else:
+                    mark(i, j, count)
+                    count += 1
+    
+    def pond_melt(pond):
+        """
+        melt the pond.
+        """           
+        rows = len(pond)
+        cols = len(pond[0])
+                
+        melted = []
+        for i in range(rows):
+            for j in range(cols):
+                val = pond[i][j]
+                if val == True:
+                    continue
+                
+                targets = [(i-1, j), (i, j-1), (i+1, j), (i, j+1)]
+                for target in targets:
+                    if (0 <= target[0] and target[0] < rows and 0 <= target[1] and target[1] < cols):
+                        target_val = pond[target[0]][target[1]]
+                        if target_val == True:
+                            melted.append((i, j))
+                            break
+        
+        for melt in melted:
+            pond[melt[0]][melt[1]] = True
+    
+    ans = 0  
+    while True:
+        pond_copy = Tester.deepcopy(pond)
+        pond_arify(pond_copy)
+        swan1 = swan_pos[0]
+        swan2 = swan_pos[1]
+        if pond_copy[swan1[0]][swan1[1]] == pond_copy[swan2[0]][swan2[1]]:
+            return ans
+        else:
+            ans += 1
+        pond_melt(pond)
+        
 
 if __name__ == "__main__":
     tester = Tester()
@@ -74,6 +194,6 @@ if __name__ == "__main__":
                                                     ".XXXXXX..XXXXXX..\n"+
                                                     "XXXXXXX...XXXX...\n"+
                                                     "..XXXXX...XXX....\n"+
-                                                    "....XXXXX.XXXL...\n"))
+                                                    "....XXXXX.XXXL..."))
     
     tester.run_cases(main_brute)
